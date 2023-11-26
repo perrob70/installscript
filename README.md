0d4de1ef1a9640ada08232a01fd19e0c129d0bb5
perrob70 <perrusch@hotmail.com>
2023-11-26 21:12:55 +0100

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # installscript
+
+This contains a personal installscript for the Debian Awesome configeration.
+Still under construction and not finished. Not to be used.
---
+++ installscriptUnfinished.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#############################
+#         Desktop
+#############################
+
+sudo apt install xorg -yy
+sudo apt install firefox-esr -yy
+sudo apt install pcmanfm -yy
+sudo apt install git -yy
+sudo apt install make -yy
+sudo apt install gcc -yy
+sudo apt install libx11-dev -yy
+sudo apt install libxft-dev -yy
+sudo apt install libxinerama-dev -yy
+sudo apt install lxappearance -yy
+sudo apt install awesome -yy
+sudo apt install mousepad-yy
+
+git clone https://github.com/perrob70/DebianAwesome
+
