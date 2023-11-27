318650a720a1385928e7781f4e0c6cc32c9e736e
perrob70 <perrusch@hotmail.com>
2023-11-27 16:42:34 +0100

    some corrections

--- installscriptUnfinished.sh
+++ installscriptUnfinished.sh
@@ -3,7 +3,8 @@
 #############################
 #         Desktop
 #############################
-sudo apt update && sudo apt upgade -yy
+sudo apt update -yy
+sudo apt upgrade -yy
 sudo apt install xorg -yy
 sudo apt install firefox-esr -yy
 sudo apt install pcmanfm -yy
@@ -15,11 +16,13 @@ sudo apt install libxft-dev -yy
 sudo apt install libxinerama-dev -yy
 sudo apt install lxappearance -yy
 sudo apt install awesome -yy
-sudo apt install mousepad-yy
-sudo apt install mpv-yy
-sudo apt install zathura-yy
-sudo apt install pycom-yy
-sudo apt install feh-yy
+sudo apt install mousepad -yy
+sudo apt install mpv -yy
+sudo apt install zathura -yy
+sudo apt install picom -yy
+sudo apt install feh -yy
 
 git clone https://github.com/perrob70/DebianAwesome
-
+mv /home/perrob/DebianAwesome/.config/awesome ~/.config/awesome
+mv /home/perrob/DebianAwesome/.config/lf ~/.config/lf
+mv /home/perrob/DebianAwesome/.config/suckless ~/.config/suckless
